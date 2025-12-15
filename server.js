9f470263f72b20ad693c2ec36b286a94dd1fc8d1
anne passelegue <annepas69@gmail.com>
2025-12-15 16:49:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 
-const express = require("express");
 const http = require("http");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost";
 
 const server = http.createServer(app);
 
-server.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
